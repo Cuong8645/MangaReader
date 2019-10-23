4ffc0bdd7da8d54d5d782316a5e0c09435c0ee2c
Cuong8645 <37360339+Cuong8645@users.noreply.github.com>
2019-10-23 13:21:31 +0700

    Add files via upload

---
+++ WebService/SignUp.php
@@ -0,0 +1,24 @@
+<?php
+ include 'DBConfig.php';
+ $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+ $json = file_get_contents('php://input');
+  $obj = json_decode($json,true);
+ $username= $obj["username"];
+  $password = $obj["password"];
+  $Sql_Query = "INSERT INTO account (`username` ,  `password` ) 
+ VALUES ('$username','$password')";
+ 
+ if(mysqli_query($con,$Sql_Query)){
+ $MSG = 'Successfully' ;
+
+ $json = json_encode($MSG);
+ echo $json ; 
+ }
+ else{
+ 
+ echo 'Wrong';
+ 
+ }
+ mysqli_close($con);
+ 
+?>
