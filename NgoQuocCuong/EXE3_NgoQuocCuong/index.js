5b3987ece8600f1963aa42b256679bbc5003c965
Cuong8645 <37360339+Cuong8645@users.noreply.github.com>
2019-10-31 12:33:36 +0700

    up

---
+++ NgoQuocCuong/EXE3_NgoQuocCuong/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import Login from './LoginScreen';
+import homepage from './homepage';
+AppRegistry.registerComponent(appName, () => App);
